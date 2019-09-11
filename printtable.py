c52537d06fd82663bfee5b0c1c146dc478ed1647
Olina Lind <olina@UG.KTH.SE>
2019-09-11 15:46:51 +0200

    powertable

--- printtable.py
+++ printtable.py
@@ -3,3 +3,14 @@ def multtable(number, limit):
         print(i * number)
 
 multtable(7, 10)
+
+import math
+
+def powertable(exponent, limit):
+    for i in range(1, limit+1):
+        power=math.pow(i, exponent)
+        i =+ 1
+        print(power)
+
+powertable(2, 4)
+powertable(3, 3)
